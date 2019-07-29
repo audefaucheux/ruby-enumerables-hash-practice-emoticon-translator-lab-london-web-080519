448da6cfb64df02c3d0b543f95e171d79f63c1da
Aude Faucheux <aude53@gmail.com>
2019-07-29 21:10:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file, emoticon)
   if result
     return result
   else return "Sorry, that emoticon was not found"
-  end  
+  end
 end
 
 def get_english_meaning(file, emoticon)
